0edbd2c37b2eb4808d961a1dad21b14e0162ef0b
Mivisha <90041857+Mivisha@users.noreply.github.com>
2021-11-11 17:36:16 +0200

    verstra

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+    
+    /* ======= Fixed header when scrolled ======= */
+    
+    $(window).bind('scroll', function() {
+         if ($(window).scrollTop() > 0) {
+             $('#header').addClass('header-scrolled');
+         }
+         else {
+             $('#header').removeClass('header-scrolled');
+         }
+    });
+    
+    /* ======= Scrollspy ======= */
+    $('body').scrollspy({ target: '#header', offset: 100});
+    
+    /* ======= ScrollTo ======= */
+    $('a.scrollto').on('click', function(e){
+        
+        //store hash
+        var target = this.hash;
+                
+        e.preventDefault();
+        
+		$('body').scrollTo(target, 800, {offset: -50, 'axis':'y'});
+        //Collapse mobile menu after clicking
+		if ($('.navbar-collapse').hasClass('in')){
+			$('.navbar-collapse').removeClass('in').addClass('collapse');
+		}
+		
+	});
+
+
+});
+//галерея
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
