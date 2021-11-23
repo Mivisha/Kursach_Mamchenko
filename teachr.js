7eef48933a4528f1d6c802f25843e8cf440ec03d
Mivisha <90041857+Mivisha@users.noreply.github.com>
2021-11-23 19:27:01 +0200

    povidom

--- teachr.js
+++ teachr.js
@@ -1,5 +1,5 @@
 var teachers =
-[{"photo":"assets/images/MK.jpg","name":"Кононенко Мадіна","posada":"Голова Академії танцю","inst":"и","facebook":"ф","desc":"?"},
-{"photo":"assets/images/MK.jpg","name":"Кононенко Мадіна","posada":"Голова Академії танцю","inst":"и","facebook":"ф","desc":"?"},
-{"photo":"assets/images/MK.jpg","name":"Кононенко Мадіна","posada":"Голова Академії танцю","inst":"и","facebook":"ф","desc":"?"},
-{"photo":"assets/images/MK.jpg","name":"Кононенко Мадіна","posada":"Голова Академії танцю","inst":"и","facebook":"ф","desc":"?"}]
+[{"photo":"assets/images/VU.png","name":"Воскресенська Єліна","posada":"Голова Академії танцю","inst":"https://www.instagram.com/voskresenskayaelina/?hl=ru","facebook":"ф","desc":"?"},
+{"photo":"assets/images/MK.png","name":"Кононенко Мадіна","posada":"Голова східного відділення","inst":"https://www.instagram.com/madina_kononenko_/?hl=ru","facebook":"ф","desc":"?"},
+{"photo":"assets/images/NN.png","name":"Назаренко Вероніка","posada":"Тренерка відділення гімнастики","inst":"https://www.instagram.com/_vero.nikka__/?hl=ru","facebook":"ф","desc":"?"},
+{"photo":"assets/images/LZ.png","name":"Зубова Валерія","posada":"Тренерка східного відділення","inst":"https://www.instagram.com/_lera_zubova_/?hl=ru","facebook":"ф","desc":"?"}]
